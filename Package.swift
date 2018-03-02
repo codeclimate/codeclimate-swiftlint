abb9f978fe7b4024727a4c69b16e13bd009799b7
Cyril Lashkevich <notorca@gmail.com>
2018-03-02 11:04:38 +0100

    Using stable release 0.25.0 of SwiftLint instead of master

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "codeclimate-SwiftLint",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/realm/SwiftLint.git", .branch("master")),
+        .package(url: "https://github.com/realm/SwiftLint.git", from:"0.25.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
